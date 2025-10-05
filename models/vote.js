f1ffe2de09a150ee1df6a2a032744f827172372e
LIL CRONE <tcronebtech2015@gmail.com>
2025-10-06 01:08:03 +0200

    Create vote.js

---
+++ models/vote.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const voteSchema = new mongoose.Schema({
+  messageId: String,
+  chatId: String,
+  image: String,
+  category: String,
+  votes: { Love: { type: Number, default: 0 }, Meh: { type: Number, default: 0 }, Not: { type: Number, default: 0 } },
+  timestamp: { type: Date, default: Date.now }
+});
+
+export const Vote = mongoose.models.Vote || mongoose.model('Vote', voteSchema);
